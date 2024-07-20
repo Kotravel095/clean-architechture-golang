d0f4c8b230e5a72cb3b8957efc86749313f607df
Jeerawat <jeerawat@orisma.com>
2024-07-20 12:58:54 +0700

    up to github

---
+++ repository/customer_mock.go
@@ -0,0 +1,29 @@
+package repository
+
+import "errors"
+
+type customerRepositoryMock struct {
+	customers []Customer
+}
+
+func NewCustomerRepositoryMock() customerRepositoryMock {
+	customers := []Customer{
+		{CustomerID:1,Name:"Jeerawat Wongsaree",Status:"1"},
+		{CustomerID:2,Name:"John Doe",Status:"1"},
+	}
+
+	return customerRepositoryMock{customers: customers}
+}
+
+func (r customerRepositoryMock) GetAll() ([]Customer, error) {
+	return r.customers, nil
+}
+
+func (r customerRepositoryMock) GetById(id int) (*Customer, error) {
+	for _, customer := range r.customers {
+		if customer.CustomerID == id {
+			return &customer, nil
+		}
+	}
+	return nil, errors.New("customer not found")
+}
